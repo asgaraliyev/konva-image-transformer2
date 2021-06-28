7f6d36533910e8f91af50d2737f9389124588fde
Asgar Aliyev <47985813+asgaraliyev@users.noreply.github.com>
2021-06-28 15:24:50 +0400

    saa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { Stage, Layer } from "react-konva";
+import ImageTransformer from "./components/ImageTransformer";
 export default function App() {
   const [isSelected, setIsSelected] = useState(false);
+  const imageUrl =
+    "https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg";
   function checkDeselect(e) {
     const didTouchStage = e.target === e.target.getStage();
     if (didTouchStage) setIsSelected(false);
@@ -12,6 +15,15 @@ export default function App() {
       onTouchStart={checkDeselect}
       width={window.innerWidth}
       height={window.innerHeight}
-    ></Stage>
+    >
+      <Layer>
+        <ImageTransformer
+          url={imageUrl}
+          width={100}
+          height={100}
+          isSelected={true}
+        />
+      </Layer>
+    </Stage>
   );
 }
